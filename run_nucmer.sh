27300aa75fae49630d214587233bc9e9f3f3b6cc
Jesus <63603068+quirozcj@users.noreply.github.com>
2022-07-20 20:02:47 +0100

    Update run_nucmer.sh

--- run_nucmer.sh
+++ run_nucmer.sh
@@ -9,7 +9,7 @@
 #SBATCH --array=0-54
 
 
-### Run whole geneome assembly mummer alignment by chromosome & filter by 20 Kbp length
+### Run whole geneome assembly mummer alignments by chromosome & filter by 20 Kbp length
 source mummer-3.23
 
 chromosome="1A"
